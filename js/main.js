034175d2f9392d1012c55e0288a6cddd7ca1c90e
Don1k1337 <dan4ik1411@gmail.com>
2022-11-09 13:49:33 +0600

    Game logic expanded -> score, restart

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeListBtn = document.querySelector('#time-list');
 const timeLeft = document.querySelector('#time');
-let time = 20;
+const board = document.querySelector('#board');
+const timeEnd = document.querySelector('.time-end')
+let time = 0;
+let score = 0;
 
 // start button actions
 startBtn.addEventListener('click', (event) => {
@@ -19,20 +22,29 @@ timeListBtn.addEventListener('click', (event) => {
     }
 })
 
-// debug
-startGame()
+board.addEventListener('click', (event) => {
+    if (event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        randomCircleGenerator()
+    }
+})
 
 // start game logic
 function setTime(timeValue) {
-    timeLeft.innerHTML = `00:${timeValue}`
+    timeLeft.innerHTML = `00:${timeValue}`;
 }
 
 function startGame() {
-    setInterval(decreaseTime, 1000)
-    setTime(time)
+    setInterval(decreaseTime, 1000);
+    randomCircleGenerator();
+    setTime(time);
 }
 
 function stopGame() {
+    board.innerHTML = `<h1>Score: <span class="primary">${score}</span></h1>`;
+    timeLeft.parentNode.classList.add('hide');
+    timeEnd.style.display = 'block'
 
 }
 
@@ -46,4 +58,22 @@ function decreaseTime() {
         }
         setTime(current)
     }
+}
+
+function getRandomSize(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomCircleGenerator() {
+    const circle = document.createElement('div');
+    const size = getRandomSize(10, 30);
+    const { width, height } = board.getBoundingClientRect()
+    const positionX = getRandomSize(0, width - size);
+    const positionY = getRandomSize(0, height - size);
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${positionY}px`
+    circle.style.left = `${positionX}px`
+    board.append(circle)
 }
